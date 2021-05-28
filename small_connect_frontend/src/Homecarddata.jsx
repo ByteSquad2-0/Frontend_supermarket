61d6f5bbee3207a99d61327308ac4432fc63c6fc
Prakhar Agrawal <prakharagrawal023@gmail.com>
2021-05-29 01:48:43 +0530

    card data added

---
+++ small_connect_frontend/src/Homecarddata.jsx
@@ -0,0 +1,40 @@
+const Homecarddata = [
+    {
+        sname = "Mangaldeep General store",
+        img = "https://static.toiimg.com/thumb/msid-81696162,width-1200,height-900,resizemode-4/.jpg",
+        ratings = "5 stars"
+
+    },
+    {
+        sname = "Mangaldeep General store",
+        img = "https://static.toiimg.com/thumb/msid-81696162,width-1200,height-900,resizemode-4/.jpg",
+        ratings = "5 stars"
+
+    },
+    {
+        sname = "Mangaldeep General store",
+        img = "https://static.toiimg.com/thumb/msid-81696162,width-1200,height-900,resizemode-4/.jpg",
+        ratings = "5 stars"
+
+    },
+    {
+        sname = "Mangaldeep General store",
+        img = "https://static.toiimg.com/thumb/msid-81696162,width-1200,height-900,resizemode-4/.jpg",
+        ratings = "5 stars"
+
+    },
+    {
+        sname = "Mangaldeep General store",
+        img = "https://static.toiimg.com/thumb/msid-81696162,width-1200,height-900,resizemode-4/.jpg",
+        ratings = "5 stars"
+
+    },
+    {
+        sname = "Mangaldeep General store",
+        img = "https://static.toiimg.com/thumb/msid-81696162,width-1200,height-900,resizemode-4/.jpg",
+        ratings = "5 stars"
+
+    }
+];
+
+export default Homecarddata;
