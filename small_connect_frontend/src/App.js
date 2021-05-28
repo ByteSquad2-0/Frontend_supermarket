0bf76a7ca9092416f940c6dc716205b2fdba4a97
AjaysinhRajput2802 <ajaysinhrajput2405@gmail.com>
2021-05-29 01:37:09 +0530

    update on footer

--- small_connect_frontend/src/App.js
+++ small_connect_frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Footer from './components/js/footer';
 
--- small_connect_frontend/src/components/js/footer.js
+++ small_connect_frontend/src/components/js/footer.js
@@ -11,66 +11,23 @@ Heading,
 const Footer = () => {
 return (
 	<Box>
-	<h1 style={{ color: "green",
-				textAlign: "center",
-				marginTop: "-50px" }}>
-		GeeksforGeeks: A Computer Science Portal for Geeks
-	</h1>
 	<Container>
 		<Row>
 		<Column>
 			<Heading>About Us</Heading>
 			<FooterLink href="#">Aim</FooterLink>
-			<FooterLink href="#">Vision</FooterLink>
-			<FooterLink href="#">Testimonials</FooterLink>
-		</Column>
-		<Column>
-			<Heading>Services</Heading>
-			<FooterLink href="#">Writing</FooterLink>
-			<FooterLink href="#">Internships</FooterLink>
-			<FooterLink href="#">Coding</FooterLink>
-			<FooterLink href="#">Teaching</FooterLink>
+			<FooterLink href="#">Team</FooterLink>
+			<FooterLink href="#">Carrers</FooterLink>
 		</Column>
 		<Column>
 			<Heading>Contact Us</Heading>
-			<FooterLink href="#">Uttar Pradesh</FooterLink>
-			<FooterLink href="#">Ahemdabad</FooterLink>
-			<FooterLink href="#">Indore</FooterLink>
-			<FooterLink href="#">Mumbai</FooterLink>
-		</Column>
-		<Column>
-			<Heading>Social Media</Heading>
-			<FooterLink href="#">
-			<i className="fab fa-facebook-f">
-				<span style={{ marginLeft: "10px" }}>
-				Facebook
-				</span>
-			</i>
-			</FooterLink>
-			<FooterLink href="#">
-			<i className="fab fa-instagram">
-				<span style={{ marginLeft: "10px" }}>
-				Instagram
-				</span>
-			</i>
-			</FooterLink>
-			<FooterLink href="#">
-			<i className="fab fa-twitter">
-				<span style={{ marginLeft: "10px" }}>
-				Twitter
-				</span>
-			</i>
-			</FooterLink>
-			<FooterLink href="#">
-			<i className="fab fa-youtube">
-				<span style={{ marginLeft: "10px" }}>
-				Youtube
-				</span>
-			</i>
-			</FooterLink>
+			<FooterLink href="mailto:small_connect@gmail.com">Mail us</FooterLink>
 		</Column>
 		</Row>
 	</Container>
+    <h5 style={{ color: "black",
+    textAlign: "center",
+    marginTop: "5px" }}>Made with &#10084; by ByteSquad Team</h5>
 	</Box>
 );
 };
