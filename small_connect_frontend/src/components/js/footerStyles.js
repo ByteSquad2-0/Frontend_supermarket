f4863f07ff8e7c9a7e05d3ad93e3f28434ebc314
Travis CI <travis>
2021-05-29 11:04:48 +0530

    add card again

--- small_connect_frontend/src/components/js/footerStyles.js
+++ small_connect_frontend/src/components/js/footerStyles.js
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 
 export const Box = styled.div`
-padding: 20px 60px 20px 60px;
+	padding: 20px 60px 20px 60px;
 background: whitesmoke;
-position: fixed;
-bottom: 0;
 width: 100%;
 
 
