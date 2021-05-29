2ea4eb638ec29a310cb369b6e0e5097f7b2f05e9
Travis CI <travis>
2021-05-29 09:39:46 +0530

    Add card component

---
+++ small_connect_frontend/src/components/card.js
@@ -0,0 +1,28 @@
+import './card.css';
+
+function Rating(props) {
+    let arr = [1,2,3,4,5];
+    return (
+        <span className="rating" >
+            {arr.map((val) => (
+                val<=props.rating ? 
+                    <i className="fa fa-star star-fill" ></i> :
+                    <i className="fa fa-star star-blank" ></i>
+            ))}
+        </span>
+    );
+}
+
+function Card(props) {
+  return (
+      <span className="card">
+          <img src={props.url} alt={props.shopname}/>
+          <span className="data">
+              <span>{props.shopname}</span>
+              <Rating rating={props.rating} />
+          </span>
+      </span>
+  );
+}
+
+export default Card;
