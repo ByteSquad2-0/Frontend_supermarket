1bab5d8477dd9c5b5b1667bacb720fc5d09bde74
Prakhar Agrawal <prakharagrawal023@gmail.com>
2021-05-30 01:48:10 +0530

    Home cards updated

--- small_connect_frontend/src/Homecarddata.jsx
+++ small_connect_frontend/src/Homecarddata.jsx
@@ -1,37 +1,37 @@
 const Homecarddata = [
     {
-        sname: "Mangaldeep General store",
-        img : "https://www.bearvalley.com/uploads/2017/07/GeneralStore_produce_summer18_banner-1920x1080.jpg",
+        sname: "Electronics Stores",
+        img : "https://relianceretail.com/img/brands/digital02.jpg",
         ratings : "5"
 
     },
     {
-        sname : "Mangaldeep General store",
+        sname : "General Stores",
         img : "https://www.bearvalley.com/uploads/2017/07/GeneralStore_produce_summer18_banner-1920x1080.jpg",
         ratings : "3"
 
     },
     {
-        sname : "Mangaldeep General store",
-        img : "https://www.bearvalley.com/uploads/2017/07/GeneralStore_produce_summer18_banner-1920x1080.jpg",
+        sname : "Grocery Stores",
+        img : "https://upload.wikimedia.org/wikipedia/commons/1/13/Supermarkt.jpg",
         ratings : "2"
 
     },
     {
-        sname : "Mangaldeep General store",
-        img : "https://www.bearvalley.com/uploads/2017/07/GeneralStore_produce_summer18_banner-1920x1080.jpg",
+        sname : "Furniture Stores",
+        img : "https://media.istockphoto.com/photos/women-buying-new-furniture-picture-id520294045?k=6&m=520294045&s=612x612&w=0&h=JcCkQ1ujGfs5MRM55nYvnA1HCUvsjQNWQ8-aVRSmwjw=",
         ratings : "1"
 
     },
     {
-        sname : "Mangaldeep General store",
-        img : "https://www.bearvalley.com/uploads/2017/07/GeneralStore_produce_summer18_banner-1920x1080.jpg",
+        sname : "Service Stores",
+        img : "https://upload.wikimedia.org/wikipedia/commons/6/66/Mechanic_and_Customer.jpg",
         ratings : "4"
 
     },
     {
-        sname : "Mangaldeep General store",
-        img : "https://www.bearvalley.com/uploads/2017/07/GeneralStore_produce_summer18_banner-1920x1080.jpg",
+        sname : "Hardware Stores",
+        img : "https://cdn2.howtostartanllc.com/images/business-ideas/business-idea-images/Hardware-Store.jpg",
         ratings : "0"
 
     }
