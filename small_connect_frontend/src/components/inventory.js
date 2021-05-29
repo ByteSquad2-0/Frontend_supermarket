93d23d428c4dbff0e38c6410e8906ab1fba0f0a4
coderg1607 <71881776+coderg1607@users.noreply.github.com>
2021-05-29 17:43:39 +0530

    Update inventory.js

--- small_connect_frontend/src/components/inventory.js
+++ small_connect_frontend/src/components/inventory.js
@@ -7,7 +7,7 @@ function Inventory(props) {
             <span className="item-name">{props.name}</span>
             <span className="item-price">Price: {props.price} Rupees</span>
         </span>
-        <button className="btn">Buy Now &nbsp;&nbsp;<i className="fa fa-arrow-right"></i></button>
+        <button className="btn">Add to cart &nbsp;&nbsp;<i className="fa fa-arrow-right"></i></button>
       </span>
   );
 }
